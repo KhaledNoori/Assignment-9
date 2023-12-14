02923d8ee41d091dd27a8ee07a7928da27c9ea22
Aliahmad <aliahmad@gmail.com>
2023-12-14 18:27:03 +0430

    adding the question 2

--- Assignment9.js
+++ Assignment9.js
@@ -1,6 +1,18 @@
 // In the name of Allah
 
+//..........................
+
+// question 2
 function sum(a,b){
     return a + b;
 }
-console.log(sum(2,4));
+// console.log(sum(2,4));
+
+// ------------------------
+// question 2
+function mystring(value){
+    return value.split('').reverse().join("");
+}
+
+console.log(mystring("ali"));
+
